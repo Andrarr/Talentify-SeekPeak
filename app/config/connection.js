f0dd00f83bc87b0a98f55f5acf11ee56a8ccaddc
Andra Rrustemi <andraa.rrustemi@gmail.com>
2022-10-20 16:56:53 +0200

    imported express

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,9 +1,8 @@
-
 import mongoose from "mongoose";
 
 const db = mongoose.connect("mongodb://localhost:27017/SeekPeak", () => {
     console.log("Database is connected!")
-},
+}, 
     err => console.error(err))
 
 
--- app/index.js
+++ app/index.js
@@ -2,13 +2,10 @@ import express from "express"
 import mongoose from "mongoose"
 const app = express()
 const PORT = process.env.PORT || 5000
-//const monnnp
 
 app.get()
 app.post()
 app.put()
 app.delete()
-
-
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
