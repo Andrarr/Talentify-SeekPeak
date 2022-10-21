22425fbdce097b7451d3acc069885d00a72a0f99
Andra Rrustemi <andraa.rrustemi@gmail.com>
2022-10-21 11:10:47 +0200

    small fix

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,9 +1,24 @@
 import mongoose from "mongoose";
+import { Cto, TeamLead, User } from "../model/users.js";
 
 const db = mongoose.connect("mongodb://localhost:27017/SeekPeak", () => {
     console.log("Database is connected!")
-}, 
+},
     err => console.error(err))
 
+async function newUser() {
+    try {
+        const user = await User.create({
 
+            email: "Johndoe@gmail.com",
+            name: "John",
+            surname: "Doe",
+            role: "User"
+        })
+        console.log(user)
+    } catch (err) {
+        console.log(err.message)
+    }
+}
 
+newUser()
