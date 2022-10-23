0dd00934ebee05e7c405462ff5ec64c79305d84e
Andra Rrustemi <andraa.rrustemi@gmail.com>
2022-10-23 14:23:13 +0200

    "departments CRUD"

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,24 +1,8 @@
 import mongoose from "mongoose";
-import { Cto, TeamLead, User } from "../model/users.js";
 
-const db = mongoose.connect("mongodb://localhost:27017/SeekPeak", () => {
+
+export const db = mongoose.connect("mongodb://localhost:27017/SeekPeak", () => {
     console.log("Database is connected!")
 },
     err => console.error(err))
 
-async function newUser() {
-    try {
-        const user = await User.create({
-
-            email: "Johndoe@gmail.com",
-            name: "John",
-            surname: "Doe",
-            role: "User"
-        })
-        console.log(user)
-    } catch (err) {
-        console.log(err.message)
-    }
-}
-
-newUser()
---
+++ app/routes/departments.js
@@ -0,0 +1,71 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { Department } from "../model/departments.js";
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+const router = express.Router()
+
+router.post("/departments", async (req, res) => {
+    if (req.body.department) {
+        try {
+            await Department.create(req.body)
+            res.send({ message: `Department has been created!` })
+            console.log(`Department has been created!`)
+
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+    else {
+        res.status(422).send({ message: `Department field is required!` })
+    }
+})
+
+router.get("/departments", (req, res) => {
+    try {
+        Department.find({}).then((function (department) {
+            res.send(department)
+        }))
+
+    } catch (err) {
+        res.json(err)
+        console.log(err.message)
+    }
+
+})
+
+router.get("/departments/:depId", (req, res) => {
+    Department.findById({ _id: req.params.depId }).then((department) => res.send(department)).catch(err => console.log(err.message))
+})
+
+router.patch("/departments/:depId", async (req, res) => {
+    try {
+        await Department.findByIdAndUpdate({ _id: req.params.depId }, req.body)
+        Department.findOne({ _id: req.params.depId }).then(function (department) {
+            res.send(department)
+            console.log(`Department with id: ${req.params.depId} has been updated!`)
+
+        })
+    } catch (err) {
+        res.json({ message: err.message })
+        console.log(err.message)
+
+    }
+})
+
+router.delete("/departments/:depId", async (req, res) => {
+    try {
+        await Department.findByIdAndRemove({ _id: req.params.depId })
+        res.json({ message: `Department with id: ${req.params.depId} has been deleted!` })
+
+    } catch (err) {
+        res.json({ message: err.message })
+        console.log(err.message)
+
+    }
+})
+
+export { router }
