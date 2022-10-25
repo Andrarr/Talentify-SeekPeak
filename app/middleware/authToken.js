6e37415de5df91424abf59db8f3df827c4981b64
Andra Rrustemi <andraa.rrustemi@gmail.com>
2022-10-25 13:23:31 +0200

    "user authorization for creating departments"

--- app/middleware/authToken.js
+++ app/middleware/authToken.js
@@ -1,18 +1,25 @@
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
-dotenv.config()
+import { User } from "../model/users.js";
+import { ObjectId } from "mongodb"
 
-export const authenticateToken = function (req, res, next) {
+
+export const authenticateToken = async (req, res, next) => {
     const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(" ")[1] //since we wanna get the second parameter of that array, array t authHeader
+    const token = authHeader && authHeader.split(" ")[1]
+    if (token == null) {
+        return res.sendStatus(401)
+    }
 
-    
-    if (token == null) return res.sendStatus(401)
+    jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`, async (err, auth) => {
+        if (err) {
+            return res.sendStatus(403)
+        }
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, member) => {
-        if (err) return res.sendStatus(403)
-        req.member = member
+        req.auth = await User.findOne({ _id: ObjectId(auth._id) });
         next()
     })
 }
+
+
 
--- app/routes/departments.js
+++ app/routes/departments.js
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { Department } from "../model/departments.js";
+import { authenticateToken } from "../middleware/authToken.js";
+import { roleAuthorization } from "../middleware/roleAuth.js"
+
+
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -8,7 +12,8 @@ app.use(bodyParser.urlencoded({
 }));
 const router = express.Router()
 
-router.post("/departments", async (req, res) => {
+router.post("/departments", roleAuthorization, async (req, res) => {
+
     if (req.body.department) {
         try {
             await Department.create(req.body)
@@ -24,7 +29,7 @@ router.post("/departments", async (req, res) => {
     }
 })
 
-router.get("/departments", (req, res) => {
+router.get("/departments", authenticateToken, (req, res) => {
     try {
         Department.find({}).then((function (department) {
             res.send(department)
@@ -37,7 +42,7 @@ router.get("/departments", (req, res) => {
 
 })
 
-router.get("/departments/:depId", (req, res) => {
+router.get("/departments/:depId", roleAuthorization, (req, res) => {
     Department.findById({ _id: req.params.depId }).then((department) => res.send(department)).catch(err => console.log(err.message))
 })
 
@@ -56,7 +61,7 @@ router.patch("/departments/:depId", async (req, res) => {
     }
 })
 
-router.delete("/departments/:depId", async (req, res) => {
+router.delete("/departments/:depId", authenticateToken, async (req, res) => {
     try {
         await Department.findByIdAndRemove({ _id: req.params.depId })
         res.json({ message: `Department with id: ${req.params.depId} has been deleted!` })
