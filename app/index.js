030aea7f5c2a3839c1898773ab777061ba9e811b
Andra Rrustemi <andraa.rrustemi@gmail.com>
2022-10-31 09:37:16 +0100

    "create team-leads(super admin route)"

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import bodyParser from "body-parser"
-import express from "express"
+import express, { query } from "express"
 import {router as depRouter} from "./routes/departments.js"
 import { db } from "./config/connection.js"
 import {router as userRouter} from "./routes/users.js"
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 4000
 import { roleAuthorization } from "./middleware/roleAuth.js"
 import {router as refresh} from "./routes/refreshToken.js"
 import cookieParser  from 'cookie-parser'
+import {user} from "./routes/super-admin.js"
+import {getApplicant} from "./routes/admin.js"
+import {newApplicants} from "./routes/applicants.js"
+import {getAllApplicants} from "./routes/admin.js"
+ import {fileUpload} from "./routes/fileManaging.js"
+import { queryApplicants} from "./routes/admin.js"
+
 
 app.use(bodyParser.json())
 
@@ -17,6 +24,11 @@ app.use("/refresh-token", refresh)
 app.use("/api", depRouter)
 app.use("/api", userRouter)
 app.use("/api", addRole)
-
+app.use("/api", user)
+app.use("/api", getApplicant)
+app.use("/api", newApplicants);
+app.use("/api", getAllApplicants )
+app.use("/upload", fileUpload)
+app.use("/query", queryApplicants)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
---
+++ app/routes/super-admin.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { Department } from "../model/departments.js";
+import { authenticateToken } from "../middleware/authToken.js";
+import { roleSuperAdmin } from "../middleware/roleAuth.js"
+import { User } from "../model/users.js";
+
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+//can manage teamLeaders, in future phases of the project
+const router = express.Router()
+
+let user = router.post("/super-admin", roleSuperAdmin, async (req, res) => {
+
+    let teamLeadToBe = {
+        name: req.body.name,
+        role: req.body.role
+    }
+    // to create a CTO or teamLeader as a USER model, the 'required' fields of sign up as user had to be commented
+    await User.create(teamLeadToBe)
+    res.send({message:"team-leader has been created"})
+})
+ export { user }
+
+
